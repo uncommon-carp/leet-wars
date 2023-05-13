fb6d9884794716855371f34c30cff6e343b702ec
Cory Sorel <116194787+uncommon-carp@users.noreply.github.com>
2023-05-13 22:52:52 +0000

    sumDigitsPower solved in JS

---
+++ sumDigitsPower/sumDigitsPow.js
@@ -0,0 +1,19 @@
+function sumDigitsPow(a, b) {
+    const result = [];
+    const diff = b - a;
+
+    for(i=a; i<=b; i++){
+        let sum = 0;
+        const digits = i.toString().split('');
+        for(x=0; x < digits.length; x++){
+            sum += Math.pow(parseInt(digits[x]), x + 1);
+        }
+        if(sum === i){
+            result.push(i)
+        }
+        
+    }
+    return result
+};
+
+console.log(sumDigitsPow(1, 100));
