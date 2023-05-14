70636a2481139634891698e7971f7af2fd73596f
Cory Sorel <csorel.creative@gmail.com>
2023-05-14 00:09:12 -0500

    cleanup

--- sumDigitsPower/sumDigitsPow.js
+++ sumDigitsPower/sumDigitsPow.js
@@ -1,19 +1,20 @@
-function sumDigitsPow(a, b) {
-    const result = [];
-    const diff = b - a;
+// https://www.codewars.com/kata/5626b561280a42ecc50000d1/
 
-    for(i=a; i<=b; i++){
-        let sum = 0;
-        const digits = i.toString().split('');
-        for(x=0; x < digits.length; x++){
-            sum += Math.pow(parseInt(digits[x]), x + 1);
-        }
-        if(sum === i){
-            result.push(i)
-        }
-        
+function sumDigitsPow(a, b) {
+  const result = [];
+
+  for (i = a; i <= b; i++) {
+    let sum = 0;
+    const digits = i.toString().split('');
+    for (x = 0; x < digits.length; x++) {
+      sum += Math.pow(parseInt(digits[x]), x + 1);
     }
-    return result
+    if (sum === i) {
+      result.push(i)
+    }
+
+  }
+  return result
 };
 
 console.log(sumDigitsPow(1, 100));
